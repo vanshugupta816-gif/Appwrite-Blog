580c21adc18ccd3e7a21271d1a5f971b7a4ec7ea
Vanshika Gupta <vanshugupta816@gmail.com>
2026-07-09 18:06:51 +0530

    update the chnages in store file

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import authSliceReducer from './AuthSlice'
 import postSliceReducer from './PostSlice'
 
-export const store = configureStore({
+export const Store = configureStore({
     reducer: {
         auth: authSliceReducer,
         post: postSliceReducer
